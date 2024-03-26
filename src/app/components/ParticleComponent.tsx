fb271627f100671c1109e27a4a2d1d36119b48b6
kotik322322 <kotan23111995@gmail.com>
2024-03-27 01:45:31 +0300

    fixed ParticlesComponent styles

--- src/app/components/ParticleComponent.tsx
+++ src/app/components/ParticleComponent.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { type Container, type ISourceOptions } from '@tsparticles/engine';
 import { loadFull } from 'tsparticles';
 import lightConfig from '../data/lightConfig.json';
 
 const ParticlesComponent = () => {
-  const [init, setInit] = useState(false);
 
   initParticlesEngine(async (engine) => {
     await loadFull(engine);
@@ -14,11 +13,11 @@ const ParticlesComponent = () => {
     console.log(container);
   };
 
-  const options: ISourceOptions = useMemo(() => lightConfig, []);
+//   const options: ISourceOptions = useMemo(() => lightConfig, []);
 
   return (
     <div className="absolute top-0 right-0 bottom-0 left-0 z-0">
-      <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={options} />
+      <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={lightConfig} />
     </div>
   );
 };
